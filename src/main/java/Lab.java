3a654bcdeb898c49ee285d1cf526bd1a79e427ae
gduke93 <grayduke93@gmail.com>
2023-03-02 21:45:10 +0000

    Completed Lab Exceptions

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -21,6 +21,7 @@ public class Lab {
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
     public void mustThrow() {
+        throw new ArithmeticException("This is the exception message");
 
     }
 }
